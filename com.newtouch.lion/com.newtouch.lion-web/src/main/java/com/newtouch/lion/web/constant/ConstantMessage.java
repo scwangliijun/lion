8c40481dc48d3aba4ca44d0dfb3ea88bc3856daa
scwanglijun <scwanglijun@gmail.com>
2014-12-26 15:25:20 +0800

    整合Spring MVC 及Freemarker

---
+++ com.newtouch.lion/com.newtouch.lion-web/src/main/java/com/newtouch/lion/web/constant/ConstantMessage.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c)  2014, Newtouch
+ * All rights reserved. 
+ *
+ * $id: ConstantMessage.java 9552 2014-3-28 下午02:05:15 WangLijun$
+ */
+package com.newtouch.lion.web.constant;
+
+/**
+ * <p>
+ * Title: 国际化常用提示信息类
+ * </p>
+ * <p>
+ * Description: 国际化常用提示信息类
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2014
+ * </p>
+ * <p>
+ * Company: Newtouch
+ * </p>
+ * 
+ * @author WangLijun
+ * @version 1.0
+ */
+public class ConstantMessage {
+	/** 添加成功 */
+	public static final String ADD_SUCCESS_MESSAGE_CODE = "common.add.success.message";
+	/** 删除成功 */
+	public static final String DELETE_SUCCESS_MESSAGE_CODE = "common.delete.success.message";
+	/** 删除失败 */
+	public static final String DELETE_FAIL_MESSAGE_CODE = "common.delete.fail.message";
+	/** 编辑成功 */
+	public static final String EDIT_SUCCESS_MESSAGE_CODE = "common.edit.success.message";
+	/** 导出成功 */
+	public static final String EXPORT_SUCCESS_MESSAGE_CODE = "common.export.success.message";
+	/** 修改的对象不能为空 */
+	public static final String EDIT_ISEMPTY_FAIL_MESSAGE_CODE = "common.edit.fail.isempty.message";
+}
